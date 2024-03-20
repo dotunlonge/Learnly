9c459aa0fe663cb31fad93d4f4af2b9d9f9b873b
Dotun Longe <oludotunlonge@gmail.com>
2024-03-20 17:29:35 +0100

    Make Hero Buttons Responsive

--- src/pages/components/home/hero.tsx
+++ src/pages/components/home/hero.tsx
@@ -1,5 +1,4 @@
-// components/home/hero.tsx
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 
 /**
@@ -16,18 +15,36 @@ import Link from 'next/link';
  * return <HeroSection />;
  */
 const HeroSection: React.FC = () => {
+  // State to hold window width
+  const [windowWidth, setWindowWidth] = useState<number>(0);
+
+  // Effect hook to set the window width on component mount and when resizing
+  useEffect(() => {
+    // Function to update the state with current window width
+    const updateWindowWidth = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    // Set window width on component mount
+    updateWindowWidth();
+
+    // Add event listener for window resize
+    window.addEventListener('resize', updateWindowWidth);
+
+    // Cleanup function to remove event listener
+    return () => window.removeEventListener('resize', updateWindowWidth);
+  }, []); // Empty dependency array ensures this runs only on mount and unmount
+
   return (
-    // Adjusted container-fluid class for full width and my-3 for margin
     <section className="p-3 bg-white text-white container-fluid d-flex justify-content-center align-items-center" id='heroSection'>
       <div className="text-center container">
-        {/* Updated headline and subheadline */}
         <h1 className="mb-3 display-4 fw-bold">Unlock Your Potential with Unlimited Learning.</h1>
         <h4 className="mb-4">Dive into expert-led courses and accelerate your career today.</h4>
-        {/* Updated CTA buttons */}
         <div className="d-flex justify-content-center">
-          <Link href="/browse-courses" className="btn btn-outline-light btn-lg me-2">Browse Courses
+          {/* Adjusted button sizes based on windowWidth state */}
+          <Link href="/browse-courses" className={`btn btn-outline-light ${windowWidth > 1024 ? 'btn-lg' : 'btn-md' } me-2`}>Browse Courses
           </Link>
-          <Link href="/join-now" className="btn btn-warning btn-lg">Join Now
+          <Link href="/join-now" className={`btn btn-warning ${windowWidth > 1024 ? 'btn-lg' : 'btn-md' }`}>Join Now
           </Link>
         </div>
       </div>
